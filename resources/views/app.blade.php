2ea136ccbf872141ef5a790e4ba61430531ed1fb
Rahmat Ilyas <rahmat.ilyas142@gmail.com>
2026-01-23 06:08:41 +0800

    Fix: Add favicon to app.blade.php using logo.png

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,9 @@
 
     <title inertia>{{ config('app.name', 'Kel. Ujung Sabbang') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/icons/logo.png') }}">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
